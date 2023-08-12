5c703072e10a6394391e454cc1f5b09e466c4222
Marius de Beer <53187487+mariusdebeer@users.noreply.github.com>
2023-08-12 11:34:48 +0200

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region     = "${var.region}"
+}
+resource "aws_vpc" "vpc1" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "Web_VPC1"
+    }
+}
+
+resource "aws_instance" "ec2_instance" {
+    ami = "${var.ami_id}"
+    count = "1"
+    instance_type = "${var.instance_type}"
+    key_name = "root_key01"
+    tags = {
+        Name = "RSAWeb_MdeBeer0729050950"
+    }
+} 
